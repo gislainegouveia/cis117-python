58d9f19d2982eb2fd7f76dfce85ba80545d3aa8b
gislainegouveia <44558837+gislainegouveia@users.noreply.github.com>
2026-08-31 20:56:10 -0700

    hello.py

--- hello.py
+++ hello.py
@@ -1,1 +1,17 @@
+# This is my first Python programming assignment
+# Author: write your name here
+# Date: write today's date here
 
+name = input("What is your name? ")
+print("Hello,", name)
+my_id = 1377953
+
+print(f"{my_id:08d}")
+print(f"{my_id:.2f}")
+print(f"{my_id:b}")
+print(f"{my_id:#x}")
+
+first = my_id // 10 ** (len(str(my_id)) - 1)
+last = my_id % 10
+
+print(first + last)
